0421216ef83019b8e71e60373d57a479b1f5251c
Sujal7ss <sujalchahande3@gmail.com>
2025-02-01 19:07:27 +0530

    backend url

--- game/src/socket.js
+++ game/src/socket.js
@@ -2,6 +2,6 @@ import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
-  process.env.NODE_ENV === "production" ? undefined : "http://localhost:8080";
+  process.env.NODE_ENV === "production" ? process.env.REACT_APP_BACKEND_URL : "http://localhost:8080";
 
 export const socket = io(URL, { autoConnect: true });
